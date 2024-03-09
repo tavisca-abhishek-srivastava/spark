64ecb0a0ae83f7d5b587b772a000d8ab1f3e8550
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-09 13:58:55 +0530

    changed file path

--- Section-3/chapter-30-RDD-Map-lambda.py
+++ Section-3/chapter-30-RDD-Map-lambda.py
@@ -1,11 +1,17 @@
 from pyspark import SparkConf , SparkContext
+
+###########################
+# this part is for python path
+import findspark
 import os
 import sys
+findspark.init()
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+###########################
 conf = SparkConf().setAppName("Map lambda")
 sc = SparkContext(master='local').getOrCreate(conf=conf)
-text = sc.textFile("c:\\tmp\\abc.txt")
+text = sc.textFile("/tmp/abc.txt")
 rdd2 = text.map(lambda x: x + " abhishek")
 print(rdd2.collect())
 
