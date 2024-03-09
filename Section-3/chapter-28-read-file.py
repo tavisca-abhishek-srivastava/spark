bb42a0e8a071c3ce480a87a2bd7b2468cb27199b
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-09 13:07:51 +0530

    changed file path

--- Section-3/chapter-28-read-file.py
+++ Section-3/chapter-28-read-file.py
@@ -4,6 +4,6 @@ conf = SparkConf().setAppName("file copy")
 
 sc = SparkContext(master='local').getOrCreate(conf=conf)
 
-rdd = sc.textFile("c:\\tmp\\abc.txt")
+rdd = sc.textFile("/tmp/abc.txt")
 print(rdd.collect())
 
