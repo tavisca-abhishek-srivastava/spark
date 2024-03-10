a464929f332d79ba8bae18a791c57faaabaf086f
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-10 14:40:59 +0530

    added comment for lambda and function. also created file in local folder

--- Section-3/chapter-36-RDD-lambda-filter.py
+++ Section-3/chapter-36-RDD-lambda-filter.py
@@ -8,7 +8,7 @@ os.system('cls||system')
 
 conf = SparkConf().setAppName("FlatMap")
 sc = SparkContext(master='local').getOrCreate(conf=conf)
-text = sc.textFile("./quiz.txt")
+text = sc.textFile("./abc-36.txt")
 rdd2 = text.filter(lambda x: x != 'Great') # if input to filter for any item is true(based upon condition) then that item is added to the output list
 print("Mapped Data: \n {0} ".format(rdd2.collect()))
 
