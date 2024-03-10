aaaedca8fd3aac38408fbe98529cc5a1c5726225
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-10 19:40:12 +0530

    added code for groupByKey

--- Section-3/RDD-GroupByKey.py
+++ Section-3/RDD-GroupByKey.py
@@ -8,11 +8,17 @@ os.system('cls||system')
 
 conf = SparkConf().setAppName("GroupByKey")
 sc = SparkContext(master='local').getOrCreate(conf=conf)
-text = sc.textFile("c:\\tmp\\rdd-disrtinct.txt")
+text = sc.textFile("./abc-40.txt")
 rdd2 = text.flatMap(lambda x: x.split(' '))
 #print("Flat Mapped Data: \n {0} ".format(rdd2.collect()))
 rdd3 = rdd2.map(lambda x: (x,len(x)+ random.randint(1,99)) )
 print(rdd3.groupByKey().mapValues(list).collect())
+
+text1 = sc.textfile("./abc-40.txt")
+rdd1 = text1.flatMap(lambda x: x.split(' '))
+rdd34 = rdd1.map(lambda x: (x,1))
+rddgbk = rdd34.groupByKey()
+print(rddgbk.collect())
 
 
 
