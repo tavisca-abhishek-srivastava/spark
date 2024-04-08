7003708d72d6aee9f07010e8705149780c427066
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-09 01:02:48 +0530

    added chapter-56

--- Section-3/chapter-56-RDD-male-female-marks.py
+++ Section-3/chapter-56-RDD-male-female-marks.py
@@ -16,10 +16,10 @@ rdd1 = rdd.filter(lambda x: x!= header)
 rdd2 = rdd.filter(lambda x: x.split(',')[1] == "Male")
 rdd3 = rdd.filter(lambda x: x.split(',')[1] == "Female")
 
-rdd2_male_numbers = rdd2.map( lambda x : (x.split(',')[1] , x.split(',')[5]))
+rdd2_male_numbers = rdd2.map( lambda x : (x.split(',')[1] , int(x.split(',')[5])))
 
-rdd3_female_numbers = rdd3.map( lambda x : (x.split(',')[1] , x.split(',')[5]))
+rdd3_female_numbers = rdd3.map( lambda x : (x.split(',')[1] , int(x.split(',')[5])))
 
-total_marks_male_rdd = rdd2_male_numbers.reduceByKey(lambda x , y: (x+y))
+total_marks_male_rdd = rdd2_male_numbers.reduceByKey(lambda x , y: (int(x)+int(y)))
 
 print(total_marks_male_rdd.collect())
