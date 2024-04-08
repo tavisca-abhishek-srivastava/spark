86a085563efc7b7c833c2f9b7be07d138b4d8409
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-08 15:26:54 +0530

    added chapter-53

--- Section-3/chapter-55-RDD-total-students.py
+++ Section-3/chapter-55-RDD-total-students.py
@@ -2,7 +2,7 @@ from pyspark import SparkConf , SparkContext
 import os
 
 conf = SparkConf().setAppName("Total-Students")
-sc = SparkContext(master='local').getorcreate(conf=conf)
+sc = SparkContext(master='local').getOrCreate(conf=conf)
 
 rdd = sc.textFile("./StudentData.csv")
 
