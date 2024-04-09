ff28526d8515191f32932320d37d6d0b9ef58db2
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-09 18:31:42 +0530

    added chapter-62

---
+++ Section-4/chapter-65-DF-first.py
@@ -0,0 +1,23 @@
+# from pyspark.sql import SparkSession
+# import os
+# import sys , math , random
+# os.environ['PYSPARK_PYTHON'] = sys.executable
+# os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+# os.system('cls||system')
+
+
+# ss = SparkSession.builder.appName("First DF App").getOrCreate()
+# df = ss.read.option("header",True).csv("./StudentData.csv")
+# print((df.show()))
+
+from pyspark.sql import SparkSession
+import os
+
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+os.system('cls||system')
+
+spark_session = SparkSession.builder.appName("First DF App").getOrCreate()
+df = spark_session.read.option("header",True).csv("./StudentData.csv")
+print(df.show())
+
