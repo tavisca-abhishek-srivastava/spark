28dd733403bbe76a91f911522058570608dd2d68
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-09 10:02:37 +0530

    added chapter-60

---
+++ Section-3/chapter-61-RDD-minimum-maximum.py
@@ -0,0 +1,17 @@
+from pyspark import SparkConf , SparkContext
+import os
+
+conf = SparkConf().setAppName("Avg-per-course")
+sc = SparkContext(master='local').getOrCreate(conf=conf)
+
+rdd = sc.textFile("./StudentData.csv")
+
+header = rdd.first()
+rdd1 = rdd.filter(lambda x: x!= header)
+rdd2 = rdd1.map(lambda x: (x.split(',')[3] , int(x.split(',')[5])))
+rdd_max = rdd2.reduceByKey(lambda x , y : (x if x > y else y))
+rdd_min = rdd2.reduceByKey(lambda x , y : (x if x < y else y))
+
+
+print("min mark per course {0}".format(rdd_min.collect()))
+print("min mark per course {0}".format(rdd_max.collect()))
