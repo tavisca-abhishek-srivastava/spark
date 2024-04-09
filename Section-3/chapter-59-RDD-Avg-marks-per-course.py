246507d35e679a56b2e70cd021b9920ea5cb820e
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-09 09:15:45 +0530

    added chapter-56

---
+++ Section-3/chapter-59-RDD-Avg-marks-per-course.py
@@ -0,0 +1,17 @@
+from pyspark import SparkConf , SparkContext
+import os
+
+conf = SparkConf().setAppName("Avg-per-course")
+sc = SparkContext(master='local').getOrCreate(conf=conf)
+
+rdd = sc.textFile("./StudentData.csv")
+
+header = rdd.first()
+rdd1 = rdd.filter(lambda x: x!= header)
+rdd2 = rdd1.map(lambda x: (x.split(',')[3] , (int(x.split(',')[5])),1))
+rdd3 = rdd2.reduceByKey(lambda x , y : (x[1][0]+y[1][0],x[1][1]+y[1][1]))
+
+rdd4 = rdd3.map(lambda x : (x[0] , x[1][0]/x[1][1]))
+
+
+print("Total marks per course {0}".format(rdd3.collect()))
