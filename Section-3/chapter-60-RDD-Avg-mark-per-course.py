a719fd01fc7c378f249a6e15d3871233f67cefa8
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-09 09:52:03 +0530

    added chapter-60

--- Section-3/chapter-60-RDD-Avg-mark-per-course.py
+++ Section-3/chapter-60-RDD-Avg-mark-per-course.py
@@ -11,7 +11,7 @@ rdd1 = rdd.filter(lambda x: x!= header)
 rdd2 = rdd1.map(lambda x: (x.split(',')[3] , (int(x.split(',')[5]),1)))
 rdd3 = rdd2.reduceByKey(lambda x , y : (x[0]+y[0],x[1]+y[1])) # x is tuple and y is tuple
 
-rdd4 = rdd3.map(lambda x : (x[0] , x[1][0]/x[1][1]))
+rdd4 = rdd3.map(lambda x : (x[0] , round(x[1][0]/x[1][1])))
 
 
 print("Avg mark per course {0}".format(rdd4.collect()))
