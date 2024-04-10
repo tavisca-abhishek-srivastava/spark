7a15f6852074662f06e601f020b00866d0bdb8cb
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-10 20:51:16 +0530

    added chapter-76

--- Section-4/chapter-76-DF-Count-Distinct-Duplicate.py
+++ Section-4/chapter-76-DF-Count-Distinct-Duplicate.py
@@ -17,17 +17,17 @@ df1 = df.withColumn("Total_Marks",fn.lit(120))
 print(df1.count())
 print(" \n")
 
-df2 = df.select("gender","age")
-print(df2.distinct().show())
+df2 = df.select("gender","course")
+print("distinct {0}".format(df2.distinct().show()))
 
 # 'dropduplicates' is to remove the duplicate value based upon column/s value
 
 df3 = df.dropDuplicates(["gender"])
-print(df3.show())
+print(" {0}".format(df3.show()))
 
 # here combination of gender and course will be considered for uniqueness
 df4 = df.dropDuplicates(["gender", "course"])
-print(df4.show())
+print("dropDuplicates {0}".format(df4.show()))
 
 
 
