dc167d03d7f131db90f9d51cbd571e2c8dbee8f7
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-10 18:31:19 +0530

    added chapter-74

--- Section-4/chapter-74-DF-Quiz.py
+++ Section-4/chapter-74-DF-Quiz.py
@@ -9,7 +9,7 @@ os.system('cls||system')
 
 ss = SparkSession.builder.appName("First DF App").getOrCreate()
 #below option is for Provided schmea
-df = ss.read.options( header='True', delemeter=',',inferSchema='True').csv("C:\\Users\\abhishek.srivastava\\vscode\work\\Spark\\Setion-4\\StudentData.csv")
+df = ss.read.options( header='True', delemeter=',',inferSchema='True').csv("./StudentData.csv")
 os.system('cls||system')
 # used or '|' operator 
 df1 = df.withColumn("Total_Marks",fn.lit(120))
@@ -18,12 +18,12 @@ df3 = df2.withColumn("average" , ((col("marks")/col("Total_Marks"))*100).cast("I
 
 df4 = df3.filter((df3.average > 80) & (df3.course == 'OOP') )
 
-print("More than 80% : \n")
+print("Students with More than 80% in OOP: \n")
 print(df4.show())
 
 print(" \n")
 
 df5 = df3.filter((df3.average > 60) & (df3.course == 'Cloud') )
 
-print("More than 60% : \n")
+print("Students with More than 60% in Cloud : \n")
 print(df5.show())
