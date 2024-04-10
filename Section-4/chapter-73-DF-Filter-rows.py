d41ee21875718675b79cbd81987e4bc83dcbeb12
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-10 14:56:13 +0530

    added chapter-73

--- Section-4/chapter-73-DF-Filter-rows.py
+++ Section-4/chapter-73-DF-Filter-rows.py
@@ -16,25 +16,25 @@ df1 = df.filter((df.course == 'DB') | (df.course == 'Cloud'))
 print(df1.show())
 
 
-# using 'is in' operator to check the condition for multiple values
+# using 'isin' operator to check the condition for multiple values
 my_courses = ['DB','Cloud','OOP','DSA']
 df2 = df.filter((df.course.isin(my_courses)))
 print(df2.show())
 # using 'startswith' operator to check the condition for  values starting with some string
 df3 = df.filter((df.course.startswith('D')))
-print(df3.show())
+print("Courses starting with D {0}".format(df3.show()))
 
 # using 'endswith' operator to check the condition for  values ending with some string/char
 df4 = df.filter((df.name.endswith('e')))
-print(df4.show())
+print("Names ending with D {0}".format(df4.show()))
 
 # using 'contains' operator to check the condition for  values contains string/char
 df5 = df.filter((df.name.contains('se')))
-print(df5.show())
+print("Names containing 'se' {0}".format(df5.show()))
 
 # using 'like' operator to check the condition for  values contains string/char
 df6 = df.filter((df.name.like('%se%')))
-print(df6.show())
+print("Names containing 'se'{0}".format(df6.show()))
 
 
 
