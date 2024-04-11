ee8a7bb46c80141b528dcfbf1399cf8630ec2bdd
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-11 11:13:04 +0530

    added chapter-80

---
+++ Section-4/chapter-82-DF-groupBy.py
@@ -0,0 +1,33 @@
+from pyspark.sql.types import StructType,StructField , IntegerType,StringType
+from pyspark.sql import SparkSession , functions as fn
+from pyspark.sql.functions import col 
+from pyspark.sql.functions import max , min , sum , avg , mean,count
+
+import os
+import sys , math , random
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+os.system('cls||system')
+
+# ss = SparkSession.builder.appName("First DF App").getOrCreate()
+# ###below option is for Provided schmea
+# df = ss.read.options( header='True', delemeter=',',inferSchema='True').csv("C:\\Users\\abhishek.srivastava\\vscode\work\\Spark\\Setion-4\\StudentData.csv")
+# os.system('cls||system')
+
+# #### apart from columns in group by all other columns will be part of aggregate function which you want to display
+# df1 = df.groupBy('gender').count()
+# print(df1.show())
+# ### filtering 
+
+# df2 = df.filter(df.gender == 'Male')
+# df3 = df2.groupBy("course","gender").agg(count('*').alias("Total Enrollment"))
+# df4 = df3.filter(col("Total Enrollment") > 70)
+# print(df4.show())
+
+spark_session = SparkSession.builder.appName("groupBy").getOrCreate()
+df = spark_session.read.options(delemeter=",",inferSchema="True",header="True").csv("./StudentData.csv")
+df1 = df.groupBy("gender").sum("marks")
+print(df1.show)
+
+
+
