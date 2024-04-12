dac1ed7b8ce49b5573b99fe0b43c88ae5387f398
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-12 17:24:03 +0530

    added chapter-118

--- Section-6/chapter-118-spark-streaming.py
+++ Section-6/chapter-118-spark-streaming.py
@@ -1,7 +1,8 @@
 from pyspark.streaming import StreamingContext
+from pyspark.sql.streaming import DataStreamReader
 from pyspark import SparkConf , SparkContext
 
-sconf = SparkConf.setAppName("first Streaming App")
+sconf = SparkConf().setAppName("first Streaming App")
 scontext = SparkContext(master='local').getOrCreate(conf=sconf)
 
 ssc = StreamingContext(scontext,1)
@@ -10,5 +11,9 @@ rdd = ssc.textFileStream("./sparkstreaming/input")
 
 rdd.pprint()
 
+rdd.saveAsTextFiles("./sparkstreaming/output")
+
+
 ssc.start()
 ssc.awaitTermination(100)
+
