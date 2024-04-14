7226b5d80260b28a2e7d9b87e58c2e9aaf6f54e6
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-15 00:38:40 +0530

    added chapter-138-loading

--- Section-7/chapter-138-reading-data-pgsql-data.py
+++ Section-7/chapter-138-reading-data-pgsql-data.py
@@ -1,30 +1,15 @@
 from pyspark.sql import SparkSession , functions as f
 from pyspark.sql.functions import lit , col , explode
-
-
-
-
-
-
-
 ##### wget https://jdbc.postgresql.org/download/postgresql-42.7.3.jar
 ss = SparkSession.builder.config("spark.driver.extraClassPath", "/tmp/postgresql-42.7.3.jar").appName("First ETL").getOrCreate()
 #ss = SparkSession.builder.config("spark.jars.packages", "/tmp/postgresql-42.7.3.jar").appName("First ETL").getOrCreate()
 ss.sparkContext.setLogLevel("WARN")
-df = ss.read.text("./abc.txt")
-df2 = df.withColumn("splitted words" , f.split("value", " "))
-df3 = df2.withColumn("words",explode("splitted words"))
-df4 = df3.groupBy("words").count()
-
-
-
-
 driver = "org.postgresql.Driver"
 url = "jdbc:postgresql://serverless-db.cluster-cfihahod2vsa.us-east-1.rds.amazonaws.com/students"
 user = "postgres"
 password = "welcome1234"
 table = "students.pyspark.pyspark_injest"
-df4.write.format("jdbc").option("driver",driver).option("url", url).option("user",user).option("password", password).option("dbtable", table).option("mode","append").load()
+df = ss.read.format("jdbc").option("driver",driver).option("url", url).option("user",user).option("password", password).option("dbtable", table).option("mode","append").load()
 
 print(df.show())
 
