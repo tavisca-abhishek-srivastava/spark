9499eacdefe46e72e24671bda962f381f5ea10e0
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-04-19 16:05:53 +0530

    added chapter-153

--- Section-7/chapter-138-loading-data-pgsql-data.py
+++ Section-7/chapter-138-loading-data-pgsql-data.py
@@ -1,12 +1,6 @@
 from pyspark.sql import SparkSession , functions as f
 from pyspark.sql.functions import lit , col , explode
 
-
-
-
-
-
-
 ##### wget https://jdbc.postgresql.org/download/postgresql-42.7.3.jar
 ss = SparkSession.builder.config("spark.driver.extraClassPath", "/tmp/postgresql-42.7.3.jar").appName("First ETL").getOrCreate()
 #ss = SparkSession.builder.config("spark.jars.packages", "/tmp/postgresql-42.7.3.jar").appName("First ETL").getOrCreate()
@@ -16,15 +10,12 @@ df2 = df.withColumn("splitted words" , f.split("value", " "))
 df3 = df2.withColumn("words",explode("splitted words"))
 df4 = df3.groupBy("words").count()
 
-
-
-
 driver = "org.postgresql.Driver"
 url = "jdbc:postgresql://serverless-db.cluster-cfihahod2vsa.us-east-1.rds.amazonaws.com/students"
 user = "postgres"
 password = "welcome1234"
-table = "students.pyspark.pyspark_injest"
-df4.write.format("jdbc").option("driver",driver).option("url", url).option("user",user).option("password", password).option("dbtable", table).option("mode","append").load()
+table = "students.pyspark.pyspark_injest1"    ### make sure that this table doesn't exist 
+df4.write.format("jdbc").option("driver",driver).option("url", url).option("user",user).option("password", password).option("dbtable", table).option("mode","append")
 
 print(df.show())
 
--- Section-8/chapter-153-DF-Spark-read-Full-load-csv.py
+++ Section-8/chapter-153-DF-Spark-read-Full-load-csv.py
@@ -5,19 +5,15 @@ from pyspark.sql import SparkSession , functions as fn
 from pyspark.sql.functions import col , when    
 import os
 import sys , math , random
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-#os.environ['HADOOP_HOME'] = "C:\\Hadoop"
-
-os.system('cls||system')
+os.system('cls||clear')
 
 ss = SparkSession.builder.appName("cdc").getOrCreate()
 
-csvname = "C:\\Users\\abhishek.srivastava\\vscode\work\\Spark\\Section-8\\LOAD00000001.csv"
+csvname = "./LOAD00000001.csv"
 #below option is for Provided schmea
 df = ss.read.options( delemeter=',',inferSchema='True').csv(csvname)
 df1 = df.withColumnRenamed("_c0" , "id").withColumnRenamed("_c1" , "FullName").withColumnRenamed("_c2" , "City")
-os.system('cls||system')
+os.system('cls||clear')
 # used or '|' operator 
-df1.write.mode("overwrite").csv("C:\\Users\\abhishek.srivastava\\vscode\work\\Spark\\Section-8\\finaloutput")
+df1.write.mode("overwrite").csv("./finaloutput")
 #print(df1.show())
