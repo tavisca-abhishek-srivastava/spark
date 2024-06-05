a52826c62e0882429ee834b33a5651c9e5ea7549
tavisca-abhishek-srivastava <abhishek.srivastava@tavisca.com>
2024-06-05 14:39:44 +0530

    chapter-65

--- Section-4/chapter-66-DF-Spark-Infer-Schema.py
+++ Section-4/chapter-66-DF-Spark-Infer-Schema.py
@@ -1,14 +1,22 @@
 from pyspark.sql import SparkSession
 import os
 import math , random , sys
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-os.system('cls||clear')
+# os.environ['PYSPARK_PYTHON'] = sys.executable
+# os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+# os.system('cls||clear')
 
 
-ss = SparkSession.builder.appName("First DF App").getOrCreate()
+# ss = SparkSession.builder.appName("First DF App").getOrCreate()
+# ss.sparkContext.setLogLevel('WARN')
+# #below option is to infer schmea
+# df = ss.read.options(inferSchema = 'True' , header='True', delemeter=',').csv("./StudentData.csv")
+# print((df.show()))
+# print((df.printSchema()))
+
+#############
+
+ss = SparkSession.builder.appName("Infer Schema").getOrCreate()
 ss.sparkContext.setLogLevel('WARN')
-#below option is to infer schmea
-df = ss.read.options(inferSchema = 'True' , header='True', delemeter=',').csv("./StudentData.csv")
-print((df.show()))
-print((df.printSchema()))
+df = ss.read.options(header='True',inferschema='True',delemeter=',').csv("./StudentData.csv")
+print(df.show())
+print(df.printSchema())
