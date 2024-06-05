69e97350d7bde106dfd10ce54b2e4205a4dd90a1
tavisca-abhishek-srivastava <abhishek.srivastava@tavisca.com>
2024-06-05 18:21:25 +0530

    chapter-70

--- Section-4/chapter-70-DF-Select-column.py
+++ Section-4/chapter-70-DF-Select-column.py
@@ -2,9 +2,24 @@ from pyspark.sql.types import StructType,StructField , IntegerType,StringType
 from pyspark.sql import SparkSession
 import os
 import sys , math , random
+from pyspark.conf import SparkConf
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 os.system('cls||clear')
+
+conf = SparkConf()
+conf.setAll(
+        [
+            (
+                "spark.master",
+                "spark://spark-master:7077"
+            ),
+            ("spark.driver.host", "local[*]"),
+            ("spark.submit.deployMode", "client"),
+            ("spark.driver.bindAddress", "0.0.0.0"),
+            ("spark.app.name", "First DF App"),
+        ] 
+    )
 
 SparkSchema = StructType([
                             StructField("age", IntegerType(),True),
@@ -16,24 +31,31 @@ SparkSchema = StructType([
                             StructField("email", StringType(),True),
                         ])
 
-ss = SparkSession.builder.appName("First DF App").getOrCreate()
+# ss = SparkSession.builder.appName("First DF App").getOrCreate()
+ss = SparkSession.builder.config(conf=conf).getOrCreate()
 ss.sparkContext.setLogLevel('WARN')
 #below option is for Provided schmea
 df = ss.read.options( header='True', delemeter=',').schema(SparkSchema).csv("./StudentData.csv")
-print((df.select("name","age").show()))
+# print((df.select("name","age").show()))
+print(df.select("name","age").show())
 # or 
-print((df.select(df.name,df.age).show()))
-
+# print((df.select(df.name,df.age).show()))
+print(df.select(df.name,df.age).show())
 #or
 from pyspark.sql.functions import col
-print((df.select(col("roll"),col("name")).show()))
+#print((df.select(col("roll"),col("name")).show()))
+print(df.select(col("roll"),col("name")).show())
 
 # to list all columns
-print((df.select('*').show()))
+# print((df.select('*').show()))
+print(df.select("*").show())
 
 # to list columns using df.columns
-print("All columns : \n {0} ".format((df.columns)))
+# print("All columns : \n {0} ".format((df.columns)))
+print("All columns : \n {0} ".format(df.columns))
 
 # filtering out columns by creating new DataFrame
+#df1 = df.select(df.columns[0:4])
 df1 = df.select(df.columns[0:4])
+
 print(df1.show())
