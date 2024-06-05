b60a0f63f0ca5f4c1579c98e46afa19581a9fd3c
tavisca-abhishek-srivastava <abhishek.srivastava@tavisca.com>
2024-06-05 16:22:10 +0530

    chapter-67

--- Section-4/chapter-67-DF-Spark-Provided-Schema.py
+++ Section-4/chapter-67-DF-Spark-Provided-Schema.py
@@ -2,26 +2,38 @@ from pyspark.sql.types import  StructType , StructField, IntegerType , StringTyp
 from pyspark.sql import SparkSession
 import os
 import sys , math , random
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-os.system('cls||clear')
+# os.environ['PYSPARK_PYTHON'] = sys.executable
+# os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+# os.system('cls||clear')
 
 ##### structType represents a row and structFiled specify a column
-customSparkSchema = StructType([
-                            StructField("age", IntegerType(),True),
-                            StructField("gender", StringType(),True),
-                            StructField("name", StringType(),True),
-                            StructField("course", StringType(),True),
-                            StructField("roll", StringType(),True),
-                            StructField("marks", IntegerType(),True),
-                            StructField("email", StringType(),True),
-                        ])
+# customSparkSchema = StructType([
+#                             StructField("age", IntegerType(),True),
+#                             StructField("gender", StringType(),True),
+#                             StructField("name", StringType(),True),
+#                             StructField("course", StringType(),True),
+#                             StructField("roll", StringType(),True),
+#                             StructField("marks", IntegerType(),True),
+#                             StructField("email", StringType(),True),
+#                         ])
+
+
+customSparkSchema1 = StructType([
+    StructField("age",IntegerType(),True),
+    StructField("gender",StringType(),True),
+    StructField("name", StringType(),True),
+    StructField("course", StringType(),True),
+    StructField("roll", StringType(),True),
+    StructField("marks", IntegerType(),True),
+    StructField("email", StringType(),True),
+])
+
 
 
 ss = SparkSession.builder.appName("my provided schema").getOrCreate()
 ss.sparkContext.setLogLevel('WARN')
 #####below option is for Provided schmea
-df = ss.read.options( header='True', delemeter=',').schema(customSparkSchema).csv("./StudentData.csv")
+df = ss.read.options( header='True', delemeter=',').schema(customSparkSchema1).csv("./StudentData.csv")
 
 print((df.show()))
 print((df.printSchema()))
